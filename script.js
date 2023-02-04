af8fbe1c02dd0ad1512f9bc7a6ecafa06145083e
NickMusaiev <it.musaiev@gmail.com>
2023-02-05 00:08:53 +0200

    firsts steps do

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-const city = document.querySelector(".city");
-let city_id = 709717;
-
-
-
+const city_write = document.querySelector(".city_write");
 document.querySelector(".go").addEventListener("click", () => {
+    console.log(city_write.value)
+    check_city(city_write.value)
+})
+
+let city = "";
+function check_city(arg) {
     fetch('./city.list.json')
         .then(res => res.json())
         .then(data =>
-
-            // if (input_city === data.)
             data.forEach(item => {
-                // console.log(item.name)
-                if (item.name.toLowerCase() == city.value.toLowerCase() && item.country == "UA") {
-                    // item.id
+                if (item.name.toLowerCase() == arg.toLowerCase() && item.country == "UA") {
                     fetch_2(item.id)
                 }
-                // console.log(111)
-                // console.log(item.id)
-                // console.log(item.id)
             })
         );
 
-    // console.log(city_id)
-
-})
-function fetch_2(temp_data) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${temp_data}&APPID=008604895bcd89f6b27f1d48846ac318`)
+}
+// https://openweathermap.org/img/wn/04n@2x.png
+const block_weather_temp = document.querySelector(".block-weather-temp");
+const city_name = document.querySelector(".city-name");
+const block_weather_humidity = document.querySelector(".block-weather-humidity");
+const icon = document.querySelector(".icon");
+function fetch_2(temp) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${temp}&APPID=008604895bcd89f6b27f1d48846ac318`)
         .then(function (response) { return response.json() })
         .then(function (data) {
-            console.log(data)
+            console.log((data.main.temp - 273).toFixed(1))
+            block_weather_temp.innerHTML = `temp ${(data.main.temp - 273).toFixed(1)}°C`;
+            city_name.innerHTML = `city ${data.name}`;
+            block_weather_humidity.innerHTML = `humidity ${data.main.humidity}%`;
+            icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"alt="">`
+
+            console.log(data);
+
+            console.log((data.main.feels_like - 273).toFixed(1))
         })
 }
 
-
 const cityes = document.querySelectorAll(".city");
 for (let key in cityes) {
-    cityes[key].addEventListener("mouseenter", () => {
+
+    cityes[key].addEventListener("mouseenter", (e) => {
         cityes[key].style.opacity = 0.8;
-        console.log(cityes[key])
+        city = e.target.id;
     }
     )
     cityes[key].addEventListener("mouseleave", () => {
         cityes[key].style.opacity = 1;
     })
-
+    cityes[key].addEventListener("click", () => {
+        check_city(city)
+    })
 }
+
